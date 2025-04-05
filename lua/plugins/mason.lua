6d5fe025b69040ec4b9765b3edbab8c774916cd5
Russell Kim <russellkim98@gmail.com>
2025-04-05 10:46:58 +0900

    fix mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,30 +2,33 @@
 
 ---@type LazySpec
 
--- In your plugin specifications (e.g., in a file under lua/user/)
 return {
-  -- ... other plugin specifications ...
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
     opts = {
       ensure_installed = {
-        "pyright", -- Or "ruff_lsp" if you're using that
-        -- Add other LSP servers you want to ensure are installed
+        "pyright",
+        -- other LSP servers
       },
       handlers = {
-        function(server_name, server_config) -- server_config now passed
+        function(server_name)
+          local lspconfig = require "lspconfig"
+          local opts = {}
+
           if server_name == "pyright" then
-            server_config.settings = server_config.settings or {}
-            server_config.settings.python = server_config.settings.python or {}
-            server_config.settings.python.debugger = server_config.settings.python.debugger or {}
-            server_config.settings.python.debugger.justMyCode = false
+            opts.settings = {
+              python = {
+                debugger = {
+                  justMyCode = false,
+                },
+              },
+            }
           end
-          return server_config
+
+          lspconfig[server_name].setup(opts)
         end,
       },
     },
   },
-
-  -- ... other plugin specifications ...
 }
